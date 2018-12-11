93e3fb2d7517fc02c1238332944635d9ce98ce28
Sergey Sharybin <sergey.vfx@gmail.com>
2018-12-11 15:53:43 +0100

    Update atomic library

--- third_party/atomic/intern/atomic_ops_msvc.h
+++ third_party/atomic/intern/atomic_ops_msvc.h
@@ -40,9 +40,14 @@
 #include <windows.h>
 #include <intrin.h>
 
-/******************************************************************************/
+#if defined (__clang__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
+#endif
+
 /* 64-bit operations. */
 #if (LG_SIZEOF_PTR == 8 || LG_SIZEOF_INT == 8)
+/* Unsigned */
 ATOMIC_INLINE uint64_t atomic_add_and_fetch_uint64(uint64_t *p, uint64_t x)
 {
 	return InterlockedExchangeAdd64((int64_t *)p, (int64_t)x) + x;
@@ -67,10 +72,37 @@ ATOMIC_INLINE uint64_t atomic_fetch_and_sub_uint64(uint64_t *p, uint64_t x)
 {
 	return InterlockedExchangeAdd64((int64_t *)p, -((int64_t)x));
 }
+
+/* Signed */
+ATOMIC_INLINE int64_t atomic_add_and_fetch_int64(int64_t *p, int64_t x)
+{
+	return InterlockedExchangeAdd64(p, x) + x;
+}
+
+ATOMIC_INLINE int64_t atomic_sub_and_fetch_int64(int64_t *p, int64_t x)
+{
+	return InterlockedExchangeAdd64(p, -x) - x;
+}
+
+ATOMIC_INLINE int64_t atomic_cas_int64(int64_t *v, int64_t old, int64_t _new)
+{
+	return InterlockedCompareExchange64(v, _new, old);
+}
+
+ATOMIC_INLINE int64_t atomic_fetch_and_add_int64(int64_t *p, int64_t x)
+{
+	return InterlockedExchangeAdd64(p, x);
+}
+
+ATOMIC_INLINE int64_t atomic_fetch_and_sub_int64(int64_t *p, int64_t x)
+{
+	return InterlockedExchangeAdd64(p, -x);
+}
 #endif
 
 /******************************************************************************/
 /* 32-bit operations. */
+/* Unsigned */
 ATOMIC_INLINE uint32_t atomic_add_and_fetch_uint32(uint32_t *p, uint32_t x)
 {
 	return InterlockedExchangeAdd(p, x) + x;
@@ -101,9 +133,41 @@ ATOMIC_INLINE uint32_t atomic_fetch_and_and_uint32(uint32_t *p, uint32_t x)
 	return InterlockedAnd((long *)p, x);
 }
 
+/* Signed */
+ATOMIC_INLINE int32_t atomic_add_and_fetch_int32(int32_t *p, int32_t x)
+{
+	return InterlockedExchangeAdd((long *)p, x) + x;
+}
+
+ATOMIC_INLINE int32_t atomic_sub_and_fetch_int32(int32_t *p, int32_t x)
+{
+	return InterlockedExchangeAdd((long *)p, -x) - x;
+}
+
+ATOMIC_INLINE int32_t atomic_cas_int32(int32_t *v, int32_t old, int32_t _new)
+{
+	return InterlockedCompareExchange((long *)v, _new, old);
+}
+
+ATOMIC_INLINE int32_t atomic_fetch_and_add_int32(int32_t *p, int32_t x)
+{
+	return InterlockedExchangeAdd((long *)p, x);
+}
+
+ATOMIC_INLINE int32_t atomic_fetch_and_or_int32(int32_t *p, int32_t x)
+{
+	return InterlockedOr((long *)p, x);
+}
+
+ATOMIC_INLINE int32_t atomic_fetch_and_and_int32(int32_t *p, int32_t x)
+{
+	return InterlockedAnd((long *)p, x);
+}
+
 /******************************************************************************/
 /* 8-bit operations. */
 
+/* Unsigned */
 #pragma intrinsic(_InterlockedAnd8)
 ATOMIC_INLINE uint8_t atomic_fetch_and_and_uint8(uint8_t *p, uint8_t b)
 {
@@ -123,5 +187,31 @@ ATOMIC_INLINE uint8_t atomic_fetch_and_or_uint8(uint8_t *p, uint8_t b)
 	return _InterlockedOr8((char *)p, (char)b);
 #endif
 }
+
+/* Signed */
+#pragma intrinsic(_InterlockedAnd8)
+ATOMIC_INLINE int8_t atomic_fetch_and_and_int8(int8_t *p, int8_t b)
+{
+#if (LG_SIZEOF_PTR == 8 || LG_SIZEOF_INT == 8)
+	return InterlockedAnd8((char *)p, (char)b);
+#else
+	return _InterlockedAnd8((char *)p, (char)b);
+#endif
+}
+
+#pragma intrinsic(_InterlockedOr8)
+ATOMIC_INLINE int8_t atomic_fetch_and_or_int8(int8_t *p, int8_t b)
+{
+#if (LG_SIZEOF_PTR == 8 || LG_SIZEOF_INT == 8)
+	return InterlockedOr8((char *)p, (char)b);
+#else
+	return _InterlockedOr8((char *)p, (char)b);
+#endif
+}
+
+
+#if defined (__clang__)
+#  pragma GCC diagnostic pop
+#endif
 
 #endif /* __ATOMIC_OPS_MSVC_H__ */
--- third_party/atomic/intern/atomic_ops_utils.h
+++ third_party/atomic/intern/atomic_ops_utils.h
@@ -66,14 +66,12 @@
 #  define ATOMIC_INLINE static inline __attribute__((always_inline))
 #endif
 
-#ifndef LIKELY
-#  ifdef __GNUC__
-#    define LIKELY(x)       __builtin_expect(!!(x), 1)
-#    define UNLIKELY(x)     __builtin_expect(!!(x), 0)
-#  else
-#    define LIKELY(x)       (x)
-#    define UNLIKELY(x)     (x)
-#  endif
+#ifdef __GNUC__
+#  define _ATOMIC_LIKELY(x)       __builtin_expect(!!(x), 1)
+#  define _ATOMIC_UNLIKELY(x)     __builtin_expect(!!(x), 0)
+#else
+#  define _ATOMIC_LIKELY(x)       (x)
+#  define _ATOMIC_UNLIKELY(x)     (x)
 #endif
 
 #if defined(__SIZEOF_POINTER__)
@@ -102,6 +100,33 @@
 #  define LG_SIZEOF_INT 8
 #else
 #  error "Cannot find int size"
+#endif
+
+/* Copied from BLI_utils... */
+/* C++ can't use _Static_assert, expects static_assert() but c++0x only,
+ * Coverity also errors out. */
+#if (!defined(__cplusplus)) && \
+    (!defined(__COVERITY__)) && \
+    (defined(__GNUC__) && ((__GNUC__ * 100 + __GNUC_MINOR__) >= 406))  /* gcc4.6+ only */
+#  define ATOMIC_STATIC_ASSERT(a, msg) __extension__ _Static_assert(a, msg);
+#else
+/* Code adapted from http://www.pixelbeat.org/programming/gcc/static_assert.html */
+/* Note we need the two concats below because arguments to ## are not expanded, so we need to
+ * expand __LINE__ with one indirection before doing the actual concatenation. */
+#  define ATOMIC_ASSERT_CONCAT_(a, b) a##b
+#  define ATOMIC_ASSERT_CONCAT(a, b) ATOMIC_ASSERT_CONCAT_(a, b)
+   /* These can't be used after statements in c89. */
+#  if defined(__COUNTER__)  /* MSVC */
+#    define ATOMIC_STATIC_ASSERT(a, msg) \
+         ; enum { ATOMIC_ASSERT_CONCAT(static_assert_, __COUNTER__) = 1 / (int)(!!(a)) };
+#  else  /* older gcc, clang... */
+    /* This can't be used twice on the same line so ensure if using in headers
+     * that the headers are not included twice (by wrapping in #ifndef...#endif)
+     * Note it doesn't cause an issue when used on same line of separate modules
+     * compiled with gcc -combine -fwhole-program. */
+#    define ATOMIC_STATIC_ASSERT(a, msg) \
+         ; enum { ATOMIC_ASSERT_CONCAT(assert_line_, __LINE__) = 1 / (int)(!!(a)) };
+#  endif
 #endif
 
 #endif /* __ATOMIC_OPS_UTILS_H__ */
