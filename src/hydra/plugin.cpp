16854e02e0dea1c291c693a9fa782f2907f351c6
Alex Fuller <mistaed>
2023-02-06 18:29:31 +0100

    USD 23.02 update
    HdRenderDelegate got a change with the interface, adding gpuSupported. It
    currently is just a dummy implementation without checking for anything
    GPU-related.
    
    Differential Revision: https://developer.blender.org/D17207

--- src/hydra/plugin.cpp
+++ src/hydra/plugin.cpp
@@ -39,10 +39,17 @@ HdCyclesPlugin::~HdCyclesPlugin()
 {
 }
 
+#if PXR_VERSION < 2302
 bool HdCyclesPlugin::IsSupported() const
 {
   return true;
 }
+#else
+bool HdCyclesPlugin::IsSupported(bool gpuEnabled) const
+{
+  return true;
+}
+#endif
 
 HdRenderDelegate *HdCyclesPlugin::CreateRenderDelegate()
 {
