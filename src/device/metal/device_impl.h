bc8b819c35067268b143d43c9adf0a0479603b36
Alaska <alaskayou01@gmail.com>
2024-06-26 17:16:20 +0200

    Remove amd and intel gpu support from metal backend
    This is because with the addition of new features to Cycles, these GPUs
    experienced significant performance regressions and bugs, all stemming
    from bugs in the Metal GPU driver/compiler. The only reasonable way to
    work around these issues was to disable parts of Cycles code on
    these GPUs to avoid the driver/compiler bugs.
    
    This resulted in increased development time maintaining these platforms
    while being unable to deliver feature parity with other
    GPU backends.
    
    It has been decided that this development time is better spent
    maintaining platforms that are still actively maintained by
    hardware/software vendors, and so AMD and Intel GPU support will be
    removed from the Metal backend for Cycles.

--- src/device/metal/device_impl.h
+++ src/device/metal/device_impl.h
@@ -54,8 +54,6 @@ class MetalDevice : public Device {
   id<MTLAccelerationStructure> accel_struct = nil;
   /*---------------------------------------------------*/
 
-  MetalGPUVendor device_vendor;
-
   uint kernel_features;
   bool using_nanovdb = false;
   MTLResourceOptions default_storage_mode;
