89561bbdcf0c7e2d48936e5fe20e8302ed4e2310
Brecht Van Lommel <brecht@blender.org>
2026-06-05 22:52:58 +0200

    Fix: Cycles: Build error without OpenColorIO
    Not a required dependency for Cycles standalone yet, even if it is for
    Blender.
    
    This issue was introduced in 5d2b8f8a529.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/159666

--- src/util/colorspace.cpp
+++ src/util/colorspace.cpp
@@ -664,6 +664,7 @@ void ColorSpaceManager::to_scene_linear(ustring colorspace,
   (void)y_stride;
   (void)is_rgba;
   (void)compress_as_srgb;
+  (void)ignore_alpha;
 #endif
 }
 
@@ -857,6 +858,7 @@ std::string ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
 
 const char *ColorSpaceManager::get_scene_linear_interop_id(const bool srgb_encoded)
 {
+#ifdef WITH_OCIO
   check_invalidate_caches();
 
   const thread_scoped_lock cache_lock(cache_scene_linear_interop_id_mutex);
@@ -885,6 +887,10 @@ const char *ColorSpaceManager::get_scene_linear_interop_id(const bool srgb_encod
   }
 
   return (srgb_encoded) ? cache_scene_linear_srgb_interop_id : cache_scene_linear_interop_id;
+#else
+  (void)srgb_encoded;
+  return "lin_rec709_scene";
+#endif
 }
 
 /* Template instantiations so we don't have to inline functions. */
